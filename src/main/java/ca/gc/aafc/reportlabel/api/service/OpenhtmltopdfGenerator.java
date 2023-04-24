0fc2988afe97bd338da007161510a3543c675080
Christian Gendreau <christiangendreau@gmail.com>
2023-04-24 16:21:08 -0400

    Make default PageUnit final

--- src/main/java/ca/gc/aafc/reportlabel/api/service/OpenhtmltopdfGenerator.java
+++ src/main/java/ca/gc/aafc/reportlabel/api/service/OpenhtmltopdfGenerator.java
@@ -11,7 +11,7 @@ import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 @Service
 public class OpenhtmltopdfGenerator implements PDFGenerator {
 
-  public static BaseRendererBuilder.PageSizeUnits pageUnit = BaseRendererBuilder.PageSizeUnits.MM;
+  public static final BaseRendererBuilder.PageSizeUnits DEFAULT_PAGE_UNIT = BaseRendererBuilder.PageSizeUnits.MM;
   private static final float DEFAULT_PAGE_WIDTH = 210;
   private static final float DEFAULT_PAGE_HEIGHT = 297;
 
@@ -21,7 +21,7 @@ public class OpenhtmltopdfGenerator implements PDFGenerator {
     PdfRendererBuilder builder = new PdfRendererBuilder();
     builder.withHtmlContent(html, baseDocumentUri);
 
-    builder.useDefaultPageSize(DEFAULT_PAGE_WIDTH, DEFAULT_PAGE_HEIGHT, pageUnit);
+    builder.useDefaultPageSize(DEFAULT_PAGE_WIDTH, DEFAULT_PAGE_HEIGHT, DEFAULT_PAGE_UNIT);
     builder.toStream(os);
     builder.run();
   }
