f33ae1fbffe03accdc2590966c12629d9967ad90
Christian Gendreau <christiangendreau@gmail.com>
2024-09-25 14:50:52 -0400

    Completed Sequence Submission file example

--- src/test/java/ca/gc/aafc/dina/export/api/repository/ReportRequestResourceRepositoryIT.java
+++ src/test/java/ca/gc/aafc/dina/export/api/repository/ReportRequestResourceRepositoryIT.java
@@ -3,8 +3,6 @@ package ca.gc.aafc.dina.export.api.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 import ca.gc.aafc.dina.export.api.BaseIntegrationTest;
 import ca.gc.aafc.dina.export.api.DinaExportModuleApiLauncher;
@@ -93,8 +91,7 @@ public class ReportRequestResourceRepositoryIT extends BaseIntegrationTest {
 
   @WithMockKeycloakUser(username = "user", groupRole = ReportRequestTestFixture.GROUP + ":USER")
   @Test
-  public void create_onSequenceSubmissionCSVReportRequest_requestAccepted()
-    throws JsonProcessingException {
+  public void create_onSequenceSubmissionCSVReportRequest_requestAccepted() {
 
     ReportTemplateDto templateDto = ReportTemplateTestFixture.newReportTemplate()
       .templateFilename("testSeqSubmissionJson.flt")
@@ -104,7 +101,7 @@ public class ReportRequestResourceRepositoryIT extends BaseIntegrationTest {
     templateDto = reportRepository.create(templateDto);
 
     Map<String, Object> payload = Map.of(
-      "seqReaction", List.of(
+      "seq-reaction", List.of(
         JsonAPITestHelper.toJsonAPIMap("seq-reaction", Map.of("", ""),
           JsonAPITestHelper.toRelationshipMap(List.of(
             JsonAPIRelationship.of("seqPrimer", "pcr-primer",
@@ -115,7 +112,10 @@ public class ReportRequestResourceRepositoryIT extends BaseIntegrationTest {
               "92513db6-d3b9-4b12-ab2d-c2c3ca57c06a"))
           ),
           "a32f1ae0-d91f-4c7d-ada6-d79e12c5a017")),
-      "seq-batch", JsonAPITestHelper.toJsonAPIMap("seq-batch", Map.of("name", "Seq batch1"),
+      "seq-batch", JsonAPITestHelper.toJsonAPIMap("seq-batch", Map.of("name", "SeqBatch1"),
+        JsonAPITestHelper.toRelationshipMap(
+            JsonAPIRelationship.of("region", "region",
+              "c35a44ce-ade3-45bd-8c67-8927bca94a8c")),
         "a7708ac4-78bd-486d-bb57-1013a8dffbf1"),
       "included", Map.of(
         "pcr-primer", List.of(JsonAPITestHelper.toJsonAPIMap("pcr-primer", Map.of("name", "primer1"), "8f1b14cd-1cf6-4bff-92c2-732b6d604576")),
@@ -125,11 +125,8 @@ public class ReportRequestResourceRepositoryIT extends BaseIntegrationTest {
               "14e6cd01-d3ca-48b3-9a9f-f1b646c2543"))),
           "92513db6-d3b9-4b12-ab2d-c2c3ca57c06a")),
         "material-sample", List.of(JsonAPITestHelper.toJsonAPIMap("material-sample", Map.of("materialSampleName", "ABC-1"), "14e6cd01-d3ca-48b3-9a9f-f1b646c2543")),
-        "region", List.of()
+        "region", List.of(JsonAPITestHelper.toJsonAPIMap("region", Map.of("symbol", "ITS"),"c35a44ce-ade3-45bd-8c67-8927bca94a8c"))
       ));
-    ObjectMapper OM = new ObjectMapper();
-
-    System.out.println(OM.writeValueAsString(payload));
 
     ReportRequestDto dto = ReportRequestTestFixture.newReportRequest()
       .reportTemplateUUID(templateDto.getUuid())
