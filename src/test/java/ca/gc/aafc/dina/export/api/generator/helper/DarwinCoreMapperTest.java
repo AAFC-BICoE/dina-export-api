32a59bda8bb03641adcc67c454e9eb9d80127130
Brandon Andre <brandon.andre@agr.gc.ca>
2026-08-07 13:58:26 -0400

    Updated the payload to have null higherGeographicalPlaces

--- src/test/java/ca/gc/aafc/dina/export/api/generator/helper/DarwinCoreMapperTest.java
+++ src/test/java/ca/gc/aafc/dina/export/api/generator/helper/DarwinCoreMapperTest.java
@@ -28,16 +28,28 @@ public class DarwinCoreMapperTest {
 
     String json = """
         {
-          "geographicPlaceNameSourceDetail": {
-            "higherGeographicPlaces": [
-              {
-                "id": "string",
-                "element": "string",
-                "placeType": "notcounty",
-                "name": "string"
-              }
-            ]
-          }
+            "geographicPlaceNameSourceDetail": {
+                "sourceUrl": "https://nominatim.openstreetmap.org/ui/details.html?osmtype=R&osmid=62650",
+                "customGeographicPlace": null,
+                "selectedGeographicPlace": {
+                    "id": "62650",
+                    "element": "R",
+                    "placeType": "ISO3166-2-lvl4",
+                    "name": "DE-HE"
+                },
+                "higherGeographicPlaces": null,
+                "stateProvince": {
+                    "id": "62650",
+                    "element": "relation",
+                    "placeType": "state",
+                    "name": "Hesse"
+                },
+                "country": {
+                    "code": "de",
+                    "name": "Germany"
+                },
+                "recordedOn": "2026-08-07T17:38:23.075989879Z"
+            }
         }
     """;
 
